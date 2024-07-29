e8db64f5028686c312e6a610110a59e5f801aa3a
Daria Ignatieva <DariTheDragon@yandex.ru>
2024-07-29 18:12:05 +0300

    Refactoring done, added --format options code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
-import { gendiff } from "../src/command-line/gendiff-command.js";
+import { program } from 'commander';
+
+const gendiff = () => {
+    program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1')
+
+    program  
+    .argument('<filepath1>', 'First file address')
+    .argument('<filepath2>', 'Second file address')
+    .option('-f ,--format <type>', 'output format')
+
+    program.parse(process.argv);
+}
 
 gendiff()
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const readFile = (pathToFile) => {
-    const text = fs.readFileSync(pathToFile, 'utf-8');
-    return text
-}
-
-export { readFile }
