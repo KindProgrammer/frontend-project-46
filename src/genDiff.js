ed2c4059d6affe60cfc70e82f4e25f9509572ecf
Daria Ignatieva <DariTheDragon@yandex.ru>
2024-09-05 17:36:33 +0300

    Changed foundDiff function

--- src/genDiff.js
+++ src/genDiff.js
@@ -13,35 +13,36 @@ const getUniqueSortedKeys = (obj1, obj2) => {
 
 const foundDiff = (obj1, obj2) => {
   const ollUniqueKeys = getUniqueSortedKeys(obj1, obj2);
+  let result;
 
   if (ollUniqueKeys.length === 0) {
-    return {};
+    result = {};
+  } else {
+    result = ollUniqueKeys.map((key) => {
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+
+      if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+        return { key, value1, status: 'deleted' };
+      }
+
+      if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+        return { key, value2, status: 'added' };
+      }
+
+      if (_.isObject(value1) && _.isObject(value2)) {
+        return { key, children: foundDiff(value1, value2), status: 'nested' };
+      }
+
+      if (_.isEqual(value1, value2)) {
+        return { key, value1, status: 'unchanged' };
+      }
+
+      return {
+        key, value1, value2, status: 'changed',
+      };
+    });
   }
-
-  const result = ollUniqueKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-
-    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      return { key, value1, status: 'deleted' };
-    }
-
-    if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      return { key, value2, status: 'added' };
-    }
-
-    if (_.isObject(value1) && _.isObject(value2)) {
-      return { key, children: foundDiff(value1, value2), status: 'nested' };
-    }
-
-    if (_.isEqual(value1, value2)) {
-      return { key, value1, status: 'unchanged' };
-    }
-
-    return {
-      key, value1, value2, status: 'changed',
-    };
-  });
 
   return result;
 };
