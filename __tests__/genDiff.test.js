45ca0f07ec697e697aaea59dc82930999b37f640
Daria Ignatieva <DariTheDragon@yandex.ru>
2024-09-14 19:41:31 +0300

    Fixed denDiff tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,12 +9,13 @@ import {
 test('gendiff', () => {
   const optionJson = { format: 'json' };
   const optionPlain = { format: 'plain' };
+  const optionStylish = { format: 'stylish' };
   const jsonFilePath1 = getFixturePath('file1.json');
   const jsonFilePath2 = getFixturePath('file2.json');
   const yamlFilePath1 = getFixturePath('file1.yaml');
   const yamlFilePath2 = getFixturePath('file2.yaml');
 
-  expect(genDiff(jsonFilePath1, jsonFilePath2)).toEqual(expectedStylishString);
+  expect(genDiff(jsonFilePath1, jsonFilePath2, optionStylish)).toEqual(expectedStylishString);
   expect(genDiff(yamlFilePath1, yamlFilePath2, optionJson)).toEqual(expectedJsonString);
   expect(genDiff(yamlFilePath1, yamlFilePath2, optionPlain)).toEqual(expectedPlainString);
 });
