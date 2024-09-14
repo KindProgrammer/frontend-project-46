588c676b39fc4838c5911e240ab54cd2de97013d
Daria Ignatieva <DariTheDragon@yandex.ru>
2024-09-14 16:02:47 +0300

    Reorganized index

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import parseFile from './parsers.js';
-import getStylish from './formatters/stylish.js';
-import getPlain from './formatters/plain.js';
-import getJson from './formatters/json.js';
 
 const getUniqueSortedKeys = (obj1, obj2) => {
   const obj1Keys = Object.keys(obj1);
@@ -13,69 +9,29 @@ const getUniqueSortedKeys = (obj1, obj2) => {
   return ollUniqueKeys;
 };
 
-const foundDiff = (obj1, obj2) => {
-  const ollUniqueKeys = getUniqueSortedKeys(obj1, obj2);
-  let result;
+const foundDiff = (obj1, obj2) => getUniqueSortedKeys(obj1, obj2).map((key) => {
+  const value1 = obj1[key];
+  const value2 = obj2[key];
 
-  if (ollUniqueKeys.length === 0) {
-    result = {};
-  } else {
-    result = ollUniqueKeys.map((key) => {
-      const value1 = obj1[key];
-      const value2 = obj2[key];
-
-      if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-        return { key, value1, status: 'deleted' };
-      }
-
-      if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-        return { key, value2, status: 'added' };
-      }
-
-      if (_.isObject(value1) && _.isObject(value2)) {
-        return { key, children: foundDiff(value1, value2), status: 'nested' };
-      }
-
-      if (_.isEqual(value1, value2)) {
-        return { key, value1, status: 'unchanged' };
-      }
-
-      return {
-        key, value1, value2, status: 'changed',
-      };
-    });
+  if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+    return { key, value1, status: 'deleted' };
   }
 
-  return result;
-};
-
-const showDiff = (firstObjPath, secondObjPath, options) => {
-  const obj1 = parseFile(firstObjPath);
-  const obj2 = parseFile(secondObjPath);
-  const { format } = options;
-  const difference = foundDiff(obj1, obj2);
-
-  let ansver;
-
-  if (Object.keys(difference).length === 0) {
-    ansver = '{}';
-  } else {
-    switch (format) {
-      case 'plain':
-        ansver = getPlain(difference);
-        break;
-
-      case 'json':
-        ansver = getJson(difference);
-        break;
-
-      default:
-        ansver = getStylish(difference);
-        break;
-    }
+  if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+    return { key, value2, status: 'added' };
   }
 
-  console.log(ansver);
-};
+  if (_.isObject(value1) && _.isObject(value2)) {
+    return { key, children: foundDiff(value1, value2), status: 'nested' };
+  }
 
-export { foundDiff, showDiff };
+  if (_.isEqual(value1, value2)) {
+    return { key, value1, status: 'unchanged' };
+  }
+
+  return {
+    key, value1, value2, status: 'changed',
+  };
+});
+
+export default foundDiff;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import { showDiff } from './genDiff.js';
+import fs from 'fs';
+import path from 'path';
+import parseData from './parsers.js';
+import foundDiff from './genDiff.js';
+import getFormatt from './formatters/index.js';
 
-export default showDiff;
+const getfileExtension = (filePath) => filePath.split('.').pop();
+const getDataForParse = (filePath) => fs.readFileSync(path.resolve(filePath), 'utf8');
+const dataParse = (dataForParse, ext) => parseData(dataForParse, ext);
+
+const genDiff = (firstObjPath, secondObjPath, options) => {
+  const firstObjExt = getfileExtension(firstObjPath);
+  const firstObjData = getDataForParse(firstObjPath);
+  const parseDataFirstObj = dataParse(firstObjData, firstObjExt);
+
+  const secondObjExt = getfileExtension(secondObjPath);
+  const secondObjData = getDataForParse(secondObjPath);
+  const parseDataSecondObj = dataParse(secondObjData, secondObjExt);
+
+  const { format } = options;
+  const difference = foundDiff(parseDataFirstObj, parseDataSecondObj);
+
+  let ansver;
+
+  if (Object.keys(difference).length === 0) {
+    ansver = '{}';
+  } else {
+    ansver = getFormatt(difference, format);
+  }
+
+  return ansver;
+};
+
+export default genDiff;
